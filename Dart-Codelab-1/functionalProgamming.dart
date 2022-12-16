6e99f0d113ab7daa9ece8da2776fa8aa930e01f7
rafael-nunes-abi <102240440+rafael-nunes-abi@users.noreply.github.com>
2022-12-16 10:19:04 -0300

    Remaining codelab 1 files

---
+++ Dart-Codelab-1/functionalProgamming.dart
@@ -0,0 +1,16 @@
+String scream(int lenght) => "A${'a' * lenght}h!";
+
+main() {
+  final values = [1, 2, 3, 5, 10, 50];
+  values.map(scream).forEach(print);
+  values.skip(1).take(3).map(scream).forEach(print);
+}
+
+/* imperative programming
+ main() {
+   final values = [1, 2, 3, 5, 10, 50];
+   for (var lenght in values) {
+     print(screm(lenght))
+  }
+}
+*/
