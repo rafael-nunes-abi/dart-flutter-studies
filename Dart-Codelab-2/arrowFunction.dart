e1b86ac3e652431bfbd65151e5311f2300a5a735
rafael-nunes-abi <102240440+rafael-nunes-abi@users.noreply.github.com>
2022-12-16 10:20:14 -0300

    First folder commit

---
+++ Dart-Codelab-2/arrowFunction.dart
@@ -0,0 +1,25 @@
+/*
+Long way
+bool hasEmpty = aListOfStrings.any((s) {
+  return s.isEmpty;
+});
+
+Arrow way
+bool hasEmpty = aListOfStrings.any((s) => s.isEmpty);
+*/
+
+class MyClass {
+  int value1 = 2;
+  int value2 = 3;
+  int value3 = 5;
+
+  // Returns the product of the above values:
+  int get product => value1 * value2 * value3;
+
+  // Adds 1 to value1:
+  void incrementValue1() => value1++;
+
+  // Returns a string containing each item in the
+  // list, separated by commas (e.g. 'a,b,c'):
+  String joinWithCommas(List<String> strings) => strings.join(",");
+}
